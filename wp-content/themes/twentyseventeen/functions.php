2210f2b34b461de86ce6e1a45e58e3e92dd8be87
Fabio mauriello <fabiomauriello@MFiMac.local>
2017-03-03 16:45:16 +0100

    Bootstrap embedded

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -390,6 +390,17 @@ add_action( 'wp_head', 'twentyseventeen_pingback_header' );
 /**
  * Display custom color CSS.
  */
+function additional_custom_styles() {
+
+    /*Enqueue The Styles*/
+    wp_enqueue_style( 'twentyseventeen', get_theme_file_uri() . '/assets/css/bootstrap.min.css' );
+    wp_enqueue_style( 'twentyseventeen2', get_theme_file_uri() . '/assets/css/bootstrap-theme.min.css' );
+    wp_enqueue_script( 'twentyseventeen', get_theme_file_uri() . '/assets/js/bootstrap.min.js' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'additional_custom_styles' );
+
 function twentyseventeen_colors_css_wrap() {
 	if ( 'custom' !== get_theme_mod( 'colorscheme' ) && ! is_customize_preview() ) {
 		return;
