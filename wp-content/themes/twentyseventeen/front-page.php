d37dce5b7a4532efa424263e8a39eed48f9bb5ce
Fabio mauriello <fabiomauriello@MFiMac.local>
2017-04-13 11:54:05 +0200

    fix

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -28,6 +28,35 @@ get_header(); ?>
 		<?php echo do_shortcode('[wp_posts_carousel template="compact.css" post_types="post" all_items="10" show_only="newest" exclude="" posts="" ordering="des" categories="" relation="and" tags="" show_title="true" show_created_date="true" show_description="true" allow_shortcodes="false" show_category="true" show_tags="false" show_more_button="true" show_featured_image="true" image_source="thumbnail" image_height="60" image_width="100" items_to_show_mobiles="1" items_to_show_tablets="3" items_to_show="3" slide_by="3" margin="5" loop="true" stop_on_hover="true" auto_play="false" auto_play_timeout="1200" auto_play_speed="800" nav="false" nav_speed="300" dots="true" dots_speed="200" lazy_load="false" mouse_drag="false" mouse_wheel="false" touch_drag="true" easing="linear" auto_height="true" custom_breakpoints=":"]');_?>
 		</div>
 	</div>
+	<script>
+		var txt = 'psicoterapia';
+		var txt2 = 'ricerca';
+		var txt3 = 'journal';
+		jQuery('.wp-posts-carousel-container .wp-posts-carousel-categories').each(function(){
+		   console.log(jQuery(this));
+		   if(jQuery(this).text().toUpperCase().indexOf(txt.toUpperCase()) != -1 && jQuery(this).text().toUpperCase().indexOf(txt2.toUpperCase()) != -1){
+		       jQuery(this).parents( ".wp-posts-carousel-container" ).addClass('ricerca');
+		   }else if(jQuery(this).text().toUpperCase().indexOf(txt.toUpperCase()) != -1 && jQuery(this).text().toUpperCase().indexOf(txt2.toUpperCase()) == -1){
+		       jQuery(this).parents( ".wp-posts-carousel-container" ).addClass('psicoterapia');
+		   }else if(jQuery(this).text().toUpperCase().indexOf(txt2.toUpperCase()) != -1){
+		   	   jQuery(this).parents( ".wp-posts-carousel-container" ).addClass('ricerca');
+		   }else if(jQuery(this).text().toUpperCase().indexOf(txt3.toUpperCase()) != -1){
+		   	   jQuery(this).parents( ".wp-posts-carousel-container" ).addClass('journal');
+		   }
+		});
+	</script>
+	<div class="row">
+		<div class="title col-md-12">
+			<h2>
+				Ultimi eventi
+			</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 car-row">	
+		<?php echo do_shortcode('[wp_posts_carousel template="compact.css" post_types="post" all_items="10" show_only="newest" exclude="" posts="" ordering="des" categories="143" relation="and" tags="" show_title="true" show_created_date="true" show_description="true" allow_shortcodes="false" show_category="true" show_tags="false" show_more_button="true" show_featured_image="true" image_source="thumbnail" image_height="60" image_width="100" items_to_show_mobiles="1" items_to_show_tablets="3" items_to_show="3" slide_by="3" margin="5" loop="true" stop_on_hover="true" auto_play="false" auto_play_timeout="1200" auto_play_speed="800" nav="false" nav_speed="300" dots="true" dots_speed="200" lazy_load="false" mouse_drag="false" mouse_wheel="false" touch_drag="true" easing="linear" auto_height="true" custom_breakpoints=":"]');_?>
+		</div>
+	</div>
 	<div class="row">
 		<div class="title col-md-12">
 			<h2>
